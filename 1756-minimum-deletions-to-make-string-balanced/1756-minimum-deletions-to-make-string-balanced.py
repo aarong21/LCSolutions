99437153155682a5a6e96623a51404c295929f01
Aaron George <88046295+aarong21@users.noreply.github.com>
2024-07-31 11:01:00 -0400

    Time: 678 ms (16.39%), Space: 19.8 MB (34.75%) - LeetHub

--- 1756-minimum-deletions-to-make-string-balanced/1756-minimum-deletions-to-make-string-balanced.py
+++ 1756-minimum-deletions-to-make-string-balanced/1756-minimum-deletions-to-make-string-balanced.py
@@ -3,6 +3,7 @@ class Solution:
         n = len(s)
         a_count_right = [0]*n
 
+        # keep track of as to right of index
         for i in range(n-2, -1, -1):
             a_count_right[i] = a_count_right[i+1]
             if s[i+1] == 'a':
@@ -11,6 +12,9 @@ class Solution:
         b_count_left = 0
         res = float('inf')
 
+        # count bs to left of index
+        # add a and b count to get deetions needed
+        # then take min to ensure lowest number of deltions needed
         for i,c in enumerate(s):
             res = min(res, b_count_left + a_count_right[i])
             if c == 'b':
