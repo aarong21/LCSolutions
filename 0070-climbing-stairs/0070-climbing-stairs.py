d5b27e517dea59e120487de5cf24d35461b31eb5
Aaron George <88046295+aarong21@users.noreply.github.com>
2024-08-05 11:56:44 -0400

    Time: 35 ms (52.13%), Space: 16.5 MB (54.05%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -12,14 +12,3 @@ class Solution:
             one, two = two, one+two
             i += 1
         return two
-
-        # if n < 2:
-        #     return 1
-        # elif n < 3:
-        #     return 2
-        # lis = [0]*n
-        # lis[0], lis[1] = 1, 2
-
-        # for i in range(2, n):
-        #     lis[i] = lis[i-1] + lis[i-2]
-        # return lis[-1]
