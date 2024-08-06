2b3d820ab961c7a57baad4391bb848f21d7b8c39
Aaron George <88046295+aarong21@users.noreply.github.com>
2024-08-06 15:49:33 -0400

    Time: 41 ms (21.14%), Space: 16.6 MB (25.02%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,24 +1,26 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        memo = {len(nums): 0, len(nums)+1: 0}
-        # houses beyond end cannot be robbed
 
-        def rob_from(n):
-            if n not in memo:
-                memo[n] = max(
-                    # rob this house and skip next
-                    nums[n]+rob_from(n+2),
-                    # skip this house
-                    rob_from(n+1)
-                )
-            return memo[n]
-        
-        return rob_from(0)
-        
-        # temp1, temp2 = 0, 0
+        temp1, temp2 = 0, 0
 
-        # for n in nums:
-        #     temp = max(temp1+n, temp2)
-        #     temp1 = temp2
-        #     temp2 = temp
-        # return temp2
+        for n in nums:
+            temp = max(temp1+n, temp2)
+            temp1 = temp2
+            temp2 = temp
+        return temp2
+# ------------------------------------
+        # memo = {len(nums): 0, len(nums)+1: 0}
+        # # houses beyond end cannot be robbed
+
+        # def rob_from(n):
+        #     if n not in memo:
+        #         memo[n] = max(
+        #             # rob this house and skip next
+        #             nums[n]+rob_from(n+2),
+        #             # skip this house
+        #             rob_from(n+1)
+        #         )
+        #     return memo[n]
+        
+        # return rob_from(0)
+        
