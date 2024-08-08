b18e3b3e89e3fe6e2afbb10d40a190ec89b58834
Aaron George <88046295+aarong21@users.noreply.github.com>
2024-08-08 14:55:41 -0400

    Time: 35 ms (75.57%), Space: 16.6 MB (95.92%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,23 +1,24 @@
-def backtrack(n, left, right, out, res):
-    # base case where left and right brace count = n
-    if left >= n and right >= n:
-        # join arr elements without separators
-        outStr = "".join(out)
-        res.append(outStr)
-    # add left brace
-    if left < n:
-        out.append("(")
-        backtrack(n, left+1, right, out, res)
-        out.pop()
-    # if right < left, add a right
-    if right < left:
-        out.append(")")
-        backtrack(n, left, right+1, out, res)
-        out.pop()
 class Solution:
+    def backtrack(self, n, left, right, out, res):
+        # base case where left and right brace count = n
+        if left >= n and right >= n:
+            # join arr elements without separators
+            outStr = "".join(out)
+            res.append(outStr)
+        # add left brace
+        if left < n:
+            out.append("(")
+            self.backtrack(n, left+1, right, out, res)
+            out.pop()
+        # if right < left, add a right
+        if right < left:
+            out.append(")")
+            self.backtrack(n, left, right+1, out, res)
+            out.pop()
+
     def generateParenthesis(self, n: int) -> List[str]:
         res = []
         out = []
-        backtrack(n, 0, 0, out, res)
+        self.backtrack(n, 0, 0, out, res)
         return res
         
