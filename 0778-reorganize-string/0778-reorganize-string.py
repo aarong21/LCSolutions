5ad3a19bf1a213f1dea59a9b8a3b149dfe576364
Aaron George <88046295+aarong21@users.noreply.github.com>
2025-06-06 14:34:07 -0400

    Time: 0 ms (100%), Space: 17.6 MB (93.77%) - LeetHub

--- 0778-reorganize-string/0778-reorganize-string.py
+++ 0778-reorganize-string/0778-reorganize-string.py
@@ -1,27 +1,27 @@
 class Solution:
     def reorganizeString(self, s: str) -> str:
-        # 1: count freq of each char
-        freq = {}
-        for char in s:
-            freq[char] = freq.get(char, 0) + 1
-        # 2: create max heap to order freq
-        max_heap = [(-count, char) for char, count in freq.items()]
-        heapq.heapify(max_heap)
-        # 3: build the return string
-        result = []
-        prev_c = None
-        prev_count = 0
+        # frequency list of char in s
+        count = defaultdict(int)
+        for c in s:
+            count[c] += 1
         
-        while max_heap:
-            count, char = heapq.heappop(max_heap)
-            result.append(char)
-            
-            if prev_count < 0:
-                heapq.heappush(max_heap, (prev_count, prev_c))
-            prev_c = char
-            prev_count = count + 1
-        # 4: return the return string
-        ret_str = "".join(result)
-        if len(ret_str) != len(s):
+        # if count(char) > len(s)//2, impossible
+        if any(freq > (len(s)+1)//2 for freq in count.values()):
             return ""
-        return ret_str
+        
+        # max heap (-freq, char)
+        heap = [(-freq, char) for char, freq in count.items()]
+        heapq.heapify(heap)
+        res = ""
+        ###
+        prev_freq = 0
+        prev_char = ''
+        while heap:
+            freq, char = heapq.heappop(heap)
+            res += char
+            freq += 1
+            if prev_freq < 0:
+                heapq.heappush(heap, (prev_freq,prev_char))
+            prev_freq, prev_char = freq, char
+        return res
+
