c5c2d08c026b6b2d9ee9faa243361351ee9cd356
Aaron George <88046295+aarong21@users.noreply.github.com>
2026-04-25 21:23:50 -0700

    Time: 3 ms (61.79%), Space: 21 MB (72.88%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,10 +1,13 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        # n = len(nums)
-        mapp = {}
+        # mapp = {}
+        # for num in nums:
+        #     mapp[num] = mapp.get(num,0)+1
+        # for num in mapp:
+        #     if mapp[num] == 1:
+        #         return num
+        # return -1
+        xor = 0
         for num in nums:
-            mapp[num] = mapp.get(num,0)+1
-        for num in mapp:
-            if mapp[num] == 1:
-                return num
-        return -1
+            xor ^= num
+        return xor
