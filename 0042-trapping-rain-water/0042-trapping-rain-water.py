adf6408b240c953d4ad4eb29327aa67e5866074d
Aaron George <88046295+aarong21@users.noreply.github.com>
2026-04-26 14:36:23 -0700

    Time: 10 ms (48.66%), Space: 21.1 MB (26.28%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,23 +1,17 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        if not height or len(height) < 3:
-            return 0
         
-        left, right = 0, len(height)-1 # two pointer
-        left_max = right_max = 0 # highest bar from both sides
-        trapped = 0
-
+        area = 0
+        left, right = 0,len(height)-1
+        left_max, right_max = height[left], height[right]
         while left < right:
-            if height[left] < height[right]:
-                if height[left] >= left_max:
-                    left_max = height[left] # update left max if new max
-                else:
-                    trapped += left_max - height[left] # add trapped 
+            if left_max < right_max:
                 left += 1
+                left_max = max(height[left], left_max)
+                area += left_max - height[left]
             else:
-                if height[right] >= right_max:
-                    right_max = height[right] # update right max
-                else:
-                    trapped += right_max-height[right] # add trapped
-                right -=1
-        return trapped
+                right -= 1
+                right_max = max(height[right], right_max)
+                area += right_max - height[right]
+
+        return area
