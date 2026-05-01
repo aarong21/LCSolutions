182ea8325caa2cec4f1698d88ba21db49fd8f9f3
Aaron George <88046295+aarong21@users.noreply.github.com>
2026-05-01 14:09:25 -0700

    Time: 0 ms (100%), Space: 19.9 MB (89.5%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -1,29 +1,48 @@
-
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        lis = []
+        # some type of bfs
+        # ans array --> add each set of children to array
+        # while node in queue, pop, and add children to queue
+
+        # if root empty: return []
+
+        # initialize ans[], queue[]
+        # initialize level = 0
+
+        # add root to queue
+        # while queue:
+            # lenq = len(queue)
+            # res.append([])
+            # for _ in range(lenq):
+                # node = queue.pop() from front
+                # res[level].append(node.val)
+                # if node.right:
+                    # enqueue node.right
+                # if node.left:
+                    # enqueue node.left
+            # level += 1
+        # return ans[]
+
         if not root:
-            return lis
-
-        queue = deque([root])
-        lis.append([root.val])
-        temp = deque()
-
-        # while nodes in queue to check children
+            return []
+        ans = []
+        queue = [root]
+        level = 0
         while queue:
-            node = queue.popleft()
-            if node.left:
-                temp.append(node.left)
-            if node.right:
-                    temp.append(node.right)
-            # once queue is empty, temp list contains whole row
-            # add temp values into lis as []
-            if not queue:
-                if temp:
-                    lis.append([n.val for n in temp])
-                # use children nodes as next queue
-                queue = temp
-                # clear temp to store new children nodes
-                temp = deque()
-
-        return lis
+            lenq = len(queue)
+            ans.append([])
+            for _ in range(lenq):
+                node = queue.pop(0)
+                ans[level].append(node.val)
+                if node.left is not None:
+                    queue.append(node.left)
+                if node.right is not None:
+                    queue.append(node.right)
+            level += 1
+        return ans
